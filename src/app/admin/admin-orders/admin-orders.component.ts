e0e844ce7c3fdb9f323ffe0763f0c7d4e6465ac2
Michael Cote <michaelcote7@outlook.com>
2021-05-28 23:46:15 -0700

    .

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -10,8 +10,20 @@ export class AdminOrdersComponent implements OnInit {
 
   orders$: any;
 
+  messages: string[] = ["hello cote", "messages here"];
+  counter: number = 0;
+
   constructor(public orderService: OrderService) { 
     this.orders$ = orderService.getOrders();
+  }
+
+  addMessage() {
+    this.counter++;
+    this.messages.push("new message" + this.counter);
+    let m = document.getElementById('mesc');
+    console.log(m.scrollTop)
+    console.log(m.scrollTopMax)
+    m.scrollTop = m.scrollHeight;
   }
 
   ngOnInit() {
