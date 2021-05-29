98efef923a2a4891f7e352e8073202fd48970425
Michael Cote <michaelcote7@outlook.com>
2021-05-29 16:50:41 -0700

    chat scroll fix

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -17,14 +17,57 @@ export class AdminOrdersComponent implements OnInit {
     this.orders$ = orderService.getOrders();
   }
 
+
   addMessage() {
     this.counter++;
-    this.messages.push("new message" + this.counter);
+    this.messages.push("new_message_" + this.counter);
+    // this.messages.push("new_message_" + this.counter + 1);
+    
     let m = document.getElementById('mesc');
-    console.log(m.scrollTop)
-    console.log(m.scrollTopMax)
-    m.scrollTop = m.scrollHeight;
+    console.log("💙 scroll TOP_1", m.scrollTop)
+    this.fixScroll();
+
+    // let m = document.getElementById('mesc');
+    
+    // let atBottom = m.scrollHeight - m.scrollTop === m.offsetHeight;
+
+    // console.log("🧡 inner hieght", m.offsetHeight) // true inner height //
+    // console.log("🧡 client hieght", m.clientHeight)
+    // console.log("💙 scroll TOP_1", m.scrollTop)
+    // console.log("💙 scroll HEIGHT_1", m.scrollHeight)
+    
+    // setTimeout(() => {
+    //   m.scrollTop = m.scrollHeight;
+      
+    //   console.log("💚 scroll TOP_2", m.scrollTop)
+    //   // console.log("💜", atBottom)
+    //   // console.log("💚 scroll HEIGHT_2", m.scrollHeight)
+    // });
+    
+    // m.scrollTop = m.scrollHeight;
+    
+    // console.log("💚 scroll TOP_2",m.scrollTop)
+    // console.log("💚 scroll HEIGHT_2",m.scrollHeight)
   }
+
+  fixScroll() {
+    let m = document.getElementById('mesc');
+    setTimeout(() => {
+      m.scrollTop = m.scrollHeight;
+      console.log("💚 scroll TOP", m.scrollTop)
+    });
+  }
+
+  removeMessage() {
+    this.counter--;
+    this.messages.pop();
+  }
+
+  resetMessage() {
+    this.counter = 0;
+    this.messages = ["hello_cote", "messages_here"];
+  }
+
 
   ngOnInit() {
   }
