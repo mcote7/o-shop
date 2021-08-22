7df17c2629a1b69e6dc9ec77349bbfc46e0e0807
Michael Cote <michaelcote7@outlook.com>
2021-08-22 03:37:32 -0700

    working before recipes

--- src/app/shared/services/nutrition.service.ts
+++ src/app/shared/services/nutrition.service.ts
@@ -14,7 +14,7 @@ export class NutritionService {
   constructor(private http: HttpClient) {}
 
   getNutritionFacts(item: string): Observable<any> {
-    return this.http.get(`https://api.edamam.com/api/nutrition-data?app_id=457c1396&app_key=df29b42c10b99b78f454a3a7f4ae3cae&nutrition-type=logging&ingr=${item}`)
+    return this.http.get(`https://api.edamam.com/api/nutrition-data?app_id=457c1396&app_key=df29b42c10b99b78f454a3a7f4ae3cae&nutrition-type=logging&ingr=5oz.${item}`)
       .pipe(catchError((error: any) => error))
   }
 }
