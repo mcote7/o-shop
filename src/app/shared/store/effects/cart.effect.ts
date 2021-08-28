f748b03bd5240f6650b0220fb1b5e6605ecbe7a7
Michael Cote <michaelcote7@outlook.com>
2021-08-28 14:29:30 -0700

    add/remove from cart store

--- src/app/shared/store/effects/cart.effect.ts
+++ src/app/shared/store/effects/cart.effect.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import { of } from "rxjs";
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
 import * as fromServices from '../../services/shopping-cart.service';
 import * as fromActions from '../actions/cart.action';
 
@@ -20,5 +20,19 @@ export class CartEffects {
       map(cart => fromActions.loadCartSuccess({cart: cart})),
       catchError(error => of(fromActions.LoadCartFail({errorMessage: error})))
     ))
-  ))
+  ));
+
+  addToCartEffect$ = createEffect(() => this.actions$.pipe(
+    ofType(fromActions.addToCart),
+    tap((value) => {this.cartService.addToCart(value.product)}),
+    map(prod => fromActions.addToCartSuccess(prod)),
+    catchError(error => of(fromActions.addToCartFail({errorMessage: error})))
+  ));
+
+  removeFromCartEffect$ = createEffect(() => this.actions$.pipe(
+    ofType(fromActions.removeFromCart),
+    tap((value) => {this.cartService.removeFromCart(value.product)}),
+    map(prod => fromActions.removeFromCartSuccess(prod)),
+    catchError(error => of(fromActions.removeFromCartFail({errorMessage: error})))
+  ));
 }
--- src/app/shared/store/reducers/cart.reducer.ts
+++ src/app/shared/store/reducers/cart.reducer.ts
@@ -21,15 +21,41 @@ const cartReducer = createReducer(
   
   on(
     fromActions.loadCart, 
-    state => ({ ...state, loaded: false, loading: true})),
+    state => ({ ...state, loaded: false, loading: true })),
   
   on(
     fromActions.LoadCartFail, 
-    (state, {errorMessage}) => ({ ...state, loaded: false, loading: false, errorMessage: errorMessage})),
+    (state, {errorMessage}) => ({ ...state, loaded: false, loading: false, errorMessage: errorMessage })),
   
   on(
     fromActions.loadCartSuccess, 
-    (state, {cart}) => ({ ...state, loaded: true, loading: false, cart: cart})),
+    (state, {cart}) => ({ ...state, loaded: true, loading: false, cart: cart })),
+  
+  // add to cart 
+  on(
+    fromActions.addToCart, 
+    (state, {product}) => ({...state, product: product })),
+  
+  on(
+    fromActions.addToCartFail, 
+    (state, {errorMessage}) => ({ ...state, errorMessage: errorMessage })),
+  
+  on(
+    fromActions.addToCartSuccess, 
+    (state, {product}) => ({ ...state, product: product })),
+  
+  // remove from cart
+  on(
+    fromActions.removeFromCart, 
+    (state, {product}) => ({...state, product: product })),
+  
+  on(
+    fromActions.removeFromCartFail, 
+    (state, {errorMessage}) => ({ ...state, errorMessage: errorMessage })),
+  
+  on(
+    fromActions.removeFromCartSuccess, 
+    (state, {product}) => ({ ...state, product: product })),
 );
 
 export function reducer(state: CartState | undefined, action: Action) {
